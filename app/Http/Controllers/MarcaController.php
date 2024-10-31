3f8d32acd081e57aec3bdbb019ebb172bef9cb47
Bruno Silva <brunosilvabrait@gmail.com>
2024-10-31 18:24:54 -0300

    Implement MarcaRepository

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Marca;
 use Illuminate\Http\Request;
+use App\Repositories\MarcaRepository;
 
 class MarcaController extends Controller
 {
@@ -15,34 +16,27 @@ class MarcaController extends Controller
 
     public function index(Request $request)
     {
-        //$marcas = Marca::all();
-        $marcas = array();
+
+        $marcaRepository = new MarcaRepository($this->marca);
 
         if($request->has('atributos_modelos')) {
-            $atributos_modelos = $request->atributos_modelos;
-            $marcas = $this->marcas->with('modelos: id,'.$atributos_modelos);
+            $atributos_modelos = 'modelos: id,'.$request->atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
         } else {
             $marcas = $this->merca->with('modelos');
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
         }
 
         if($request->has('filtro')){
-            $filtros = explode(';', $request->filtro);
-            foreach($filtros as $key => $condicoes){
-                $c = explode(':', $condicoes);
-                $marcas = $marcas->where($c[0], $c[1], $c[2]);
-            }
+            $marcaRepository->filtro($request->filtro);
         }
 
         if($request->has('atributos')){
-            $atributos = $request->atributos;
-            $marcas = $marcas->selectRaw($atributos)->get();
-        } else {
-            $marcas = $marcas-->get();
+            $marcaRepository->selectAtributes($request->atributos);
         }
-
         //return response()->json($marcas = $this->marca->with('modelos')->get(), 200);
 
-        return response()->json($marcas, 200);
+        return response()->json($marcaRepository->getResultado(), 200);
     }
 
     public function create()
