94be4a9045c39e9da7debc7d7b97de5268398fed
Bruno Silva <brunosilvabrait@gmail.com>
2024-11-04 19:48:56 -0300

    Carros CRUD

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Carro extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['modelo_id', 'placa', 'disponivel', 'km'];
+
+    public function rules(){
+        return [
+            'marca_id'      => 'exists:modelos,id',
+            'placa'         => 'required',
+            'diaponivel'    => 'required',//pode colocar mis tipos de extensão de arquivo
+            'km'            => 'required',//(1,2,3,4,5)
+        ];
+    }
+
+    public function modelo (){
+        return $this->belongsTo('App\Models\Modelo');
+    }
 }
