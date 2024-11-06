d30955178c229c82614f842f0c274770fa97442b
Bruno Silva <brunosilvabrait@gmail.com>
2024-11-06 19:26:58 -0300

    routes and crontroller config (JWT Auth)

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,8 @@ Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
 Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
 
 //Route::apiResource('cliente', 'ClienteController');
+
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+Route::post('me', 'App\Http\Controllers\AuthController@me');
