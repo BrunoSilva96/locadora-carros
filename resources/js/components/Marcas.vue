3ed2cd774522246d2a8988cc57e0a185e2e5b20d
Bruno Silva <brunosilvabrait@gmail.com>
2024-11-14 17:23:15 -0300

    search form for marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -3,10 +3,24 @@
       <div class="row justify-content-center">
           <div class="col-md-8">
               <div class="card">
-                  <div class="card-header">Marcas</div>
+                  <div class="card-header">Busca de marcas</div>
 
                   <div class="card-body"> 
-                    <p>Vamos manipular os registros de marca por aqui.</p>
+                    <div class="row">
+                      <div class="col mb-3">
+                        <label for="inputId" class="form-label">ID</label>
+                        <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                        <div id="idHelp" class="form-text">Opcional. Informe o ID do registro.</div>
+                      </div>
+                      <div class="col mb-3">
+                        <label for="inputNome" class="form-label">Nome</label>
+                        <input type="text" class="form-control"Nome="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                        <div id="nomeHelp" class="form-text">Opcional. Informe o Nome da marca.</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-footer">
+                    <button type="submit" class="btn btn-primary btn-sm float-end">Pesquisar</button>
                   </div>
               </div>
           </div>
