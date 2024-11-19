b55493c3b14773e1dc95338ddfb156bcc241665d
Bruno Silva <brunosilvabrait@gmail.com>
2024-11-19 19:49:46 -0300

    Create marcas OK

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -42,22 +42,56 @@
         <template v-slot:conteudo>
           <div class="form-group">
               <input-container-component titulo="Nome" id="novoNome"idHelp="novoNomeHelp"texto-ajuda="Informe o Nome da marca.">
-                <input type="text" class="form-control"Nome="novoNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca">
+                <input type="text" class="form-control"Nome="novoNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca" v-model="nomeMarca">
               </input-container-component>
           </div>
           <div class="form-group">
               <input-container-component titulo="Imagem" id="novoImagem"idHelp="novoImagemHelp"texto-ajuda="Selecione uma imagem no formato PNG.">
-                <input type="file" class="form-control"Nome="novoImagem" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem">
+                <input type="file" class="form-control"Nome="novoImagem" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem" @change="carregarImagem($event)">
               </input-container-component>
           </div>
         </template>
         <template v-slot:rodape>
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-          <button type="button" class="btn btn-primary">Salvar</button>
+          <button type="button" class="btn btn-primary" @click="salvar">Salvar</button>
         </template>
       </modal-component>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        urlBase: 'http://127.0.0.1:8000/api/v1/marca',
+        nomeMarca: '',
+        arquivoImagem: []
+      }
+    },
+    methods: {
+      carregarImagem(e) {
+        this.arquivoImagem = e.target.files
+      },
+      salvar() {
+        let formData = new FormData();
+        formData.append('nome', this.nomeMarca)
+        formData.append('imagem', this.arquivoImagem[0])
+
+        let config = {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'Accept': 'application/json'
+          }
+        }
+
+        axios.post(this.urlBase, formData, config)
+          .then(response => {
+            console.log(response);
+            
+          })
+      }
+    }
+  }
 </script>
