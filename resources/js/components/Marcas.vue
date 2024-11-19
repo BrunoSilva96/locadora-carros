54d09e243f8117aadf61d00d67e118d63be740b3
Bruno Silva <brunosilvabrait@gmail.com>
2024-11-19 19:19:28 -0300

    Modal

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -13,8 +13,8 @@
 
                   </div>
                   <div class="col mb-3">
-                    <input-container-component titulo="Nome" id="inputNome"idHelp="idHelp"texto-ajuda="Opcional. Informe o Nome da marca.">
-                    <input type="text" class="form-control"Nome="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                    <input-container-component titulo="Nome da marca" id="inputNome"idHelp="nomeHelp"texto-ajuda="Opcional. Informe o Nome da marca.">
+                      <input type="text" class="form-control"Nome="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
                     </input-container-component>
                   </div>
                 </div>
@@ -32,12 +32,30 @@
                 <table-component></table-component>
               </template>
               <template v-slot:rodape>
-                <button type="button" class="btn btn-primary btn-sm float-end">Adicionar</button>
+                <button type="button" class="btn btn-primary btn-sm float-end" data-bs-toggle="modal" data-bs-target="#modalMarca">Adicionar</button>
               </template>
             </card-component>  
             <!-- Fim do card de listagem de marcas -->
           </div>
       </div>
+      <modal-component id="modalMarca" titulo="Adicionar marca">
+        <template v-slot:conteudo>
+          <div class="form-group">
+              <input-container-component titulo="Nome" id="novoNome"idHelp="novoNomeHelp"texto-ajuda="Informe o Nome da marca.">
+                <input type="text" class="form-control"Nome="novoNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca">
+              </input-container-component>
+          </div>
+          <div class="form-group">
+              <input-container-component titulo="Imagem" id="novoImagem"idHelp="novoImagemHelp"texto-ajuda="Selecione uma imagem no formato PNG.">
+                <input type="file" class="form-control"Nome="novoImagem" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem">
+              </input-container-component>
+          </div>
+        </template>
+        <template v-slot:rodape>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+          <button type="button" class="btn btn-primary">Salvar</button>
+        </template>
+      </modal-component>
   </div>
 </template>
 
