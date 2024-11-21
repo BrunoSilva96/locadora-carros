b38c7ed429171c46bd962aca42085d51d15f50e9
Bruno Silva <brunosilvabrait@gmail.com>
2024-11-21 16:51:23 -0300

    created alert component p1

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -39,6 +39,12 @@
           </div>
       </div>
       <modal-component id="modalMarca" titulo="Adicionar marca">
+
+        <template v-slot:alertas>
+          <alert-component tipo="success"></alert-component>
+          <alert-component tipo="danger"></alert-component>
+        </template>
+
         <template v-slot:conteudo>
           <div class="form-group">
               <input-container-component titulo="Nome" id="novoNome"idHelp="novoNomeHelp"texto-ajuda="Informe o Nome da marca.">
